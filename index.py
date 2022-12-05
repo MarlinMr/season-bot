87f88448c27f776fcce799fe41e5a044c273d005
MarlinMr <gitlab@marlinmr.se>
2022-12-05 21:01:55 +0100

    functioning bot

--- index.py
+++ index.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import discord, json
+import discord, json, os, asyncio
+from discord.ext import commands
 
 with open('.secrets.json', 'r',encoding='utf8') as f:
     data = json.load(f)
     token = data["token"]
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
-client.run(token)
+bot = commands.Bot(command_prefix='€', case_insensitive=True, intents=intents)
+
+async def load():
+    for guild in bot.guilds:
+        print(f'Logged in as: {bot.user.name} in {guild.name}. Version: {discord.__version__}')
+    print("Loading cogs...")
+    for file in os.listdir('./cogs'):
+        if file.endswith("jul.py"):
+            await bot.load_extension(f"cogs.{file[:-3]}")
+
+async def main():
+    print("Starting bot...")
+    await load()
+    await bot.start(token, reconnect=True)
+
+asyncio.run(main())
