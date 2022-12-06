bcee77a20925c60a79ee84b6129b6edf9a04c5f2
MarlinMr <gitlab@marlinmr.se>
2022-12-06 21:55:59 +0100

    julebot

--- cogs/jul.py
+++ cogs/jul.py
@@ -13,9 +13,33 @@ class jul(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def gigave(self, ctx):
+    async def juleinfo(self, ctx):
+        """Informasjon fra Nissen"""
+        logging.info(f"From `{ctx.message.author}` `{ctx.message.content}` in `{ctx.message.channel}`")
+        await ctx.send("🎅: HoHo! Her kan du legge gaver i sekken med `€gigave`. Gjør du det, havner du også på lista over snille og slemme.")
+
+    @commands.command()
+    async def titt(self, ctx):
+        """Sniktitt i sekken til nissen"""
+        logging.info(f"From `{ctx.message.author}` `{ctx.message.content}` in `{ctx.message.channel}`")
+        with open("./jul/gaveliste.json", 'r', encoding='utf-8') as f:
+            data = json.load(f)
+#        gave = data["gaver"][int(random()*len(data["gaver"])-1)]
+#        await ctx.send(f"Du ser {gave}. Det er {len(data['gaver'])} gaver i sekken.")
+        await ctx.send(f"Sniktittere havner på slemmelista. Det er {len(data['gaver'])} gaver i sekken, {len(data['slemme'])} på slemmelista.")
+
+    @commands.command()
+    async def gammeltitt(self, ctx):
+        """Sniktitt i den gamle sekken"""
+        logging.info(f"From `{ctx.message.author}` `{ctx.message.content}` in `{ctx.message.channel}`")
+        with open("./jul/liste.json", 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        gave = data["gave"][int(random()*len(data["gave"])-1)]
+        await ctx.send(f"Du ser {gave}. Det er {len(data['gave'])} gaver i sekken.")
+
+    @commands.command()
+    async def gigave(self, ctx, gave):
         """Legger en gave fra deg i sekken"""
-        await ctx.send("🎅: HoHo! Nissemor og Nissefar har pussa opp postmottaket sitt, men er ikke helt ferdig enda. Du kan derimot delta i denne betatesten!")
         logging.info(f"From `{ctx.message.author}` `{ctx.message.content}` in `{ctx.message.channel}`")
         with open("./jul/gaveliste.json", 'r', encoding='utf-8') as f:
             data = json.load(f)
@@ -27,10 +51,10 @@ class jul(commands.Cog):
             with open("./jul/gaveliste.json", 'w', encoding='utf-8') as f:
                 json.dump(data,f)
                 await ctx.message.add_reaction("📜")
-        if (ctx.message.content[8:] in data["gaver"]):
+        gift = str(ctx.message.content[8:]) + " fra <@" + str(ctx.message.author.id) + ">"
+        if (gift in data["gaver"]):
             await ctx.message.add_reaction("♻️")
         else:
-            gift = str(ctx.message.content[8:]) + " fra <@" + str(ctx.message.author.id) + ">"
             data["gaver"].append(gift)
             with open("./jul/gaveliste.json", 'w', encoding='utf-8') as f:
                 json.dump(data,f)
@@ -39,7 +63,14 @@ class jul(commands.Cog):
     @commands.command()
     async def tagave(self, ctx):
         """Gir gave til de snille barna"""
+        logging.info(f"From `{ctx.message.author}` `{ctx.message.content}` in `{ctx.message.channel}`")
         await ctx.send("🎅: HoHo! Ingen gaver før julaften!")
+
+    @commands.command()
+    async def tos(self,ctx):
+        """Printer TOS"""
+        logging.info(f"From `{ctx.message.author}` `{ctx.message.content}` in `{ctx.message.channel}`")
+        await ctx.send("🎅: HoHo! Ved å legge gave i sekken med €giGave kommandoen, forplikter du å skaffe gaven. Den som får gaven har krav på å få den tilsendt fra deg innen nyttårsaften 2022. Hvis du ikke har råd til gaven du gav, kan du ta opp lån.")
 
 async def setup(bot):
     n = jul(bot)
